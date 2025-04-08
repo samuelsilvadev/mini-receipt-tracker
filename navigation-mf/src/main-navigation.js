6ffe668ee3d09aa67d77e9577ddabf87b083702d
Samuel Silva <samuelsilvawb@gmail.com>
2025-04-08 08:14:28 +0100

    feat: add tailwind to navigation

--- navigation-mf/src/main-navigation.js
+++ navigation-mf/src/main-navigation.js
@@ -1,7 +1,6 @@
-import "./style.css";
-
 const EVENT_TYPES = {
   INIT: "INIT_NAVIGATION",
+  NAVIGATE: "NAVIGATE",
 };
 
 window.addEventListener("message", (event) => {
@@ -15,19 +14,33 @@ const $wrapper = document.getElementById("navigation-mf");
 function render(data = []) {
   // TODO: sanitize data
 
-  const $list = document.createElement("ul");
-  $list.classList.add("navigation-mf-list");
+  const $list = document.getElementById("navigation-list-mf");
 
   data.forEach((item) => {
     const $li = document.createElement("li");
     const $a = document.createElement("a");
 
     $a.href = item.url;
+
+    $a.classList.add("flex");
+    $a.classList.add("items-center");
+    $a.classList.add("text-gray-600");
+    $a.classList.add("hover:text-indigo-600");
+    $a.classList.add("transition-colors");
+
     $a.textContent = item.title;
+
+    $a.addEventListener("click", (event) => {
+      event.preventDefault();
+      sendNavigation(item);
+    });
 
     $li.appendChild($a);
     $list.appendChild($li);
   });
 
-  $wrapper.appendChild($list);
+}
+
+function sendNavigation(data) {
+  window.parent.postMessage({ type: EVENT_TYPES.NAVIGATE, payload: data }, "*");
 }
